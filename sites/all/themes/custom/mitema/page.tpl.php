c878e8a482411a3fa9e3219940c07fcf804f8889
imvalle <imenendezvalle@gmail.com>
2016-05-06 08:32:52 +0200

    Desde ofi con netbeans

---
+++ sites/all/themes/custom/mitema/page.tpl.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+
+		<!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame
+		Remove this if you use the .htaccess -->
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+		<title>mitema</title>
+		<meta name="description" content="">
+		<meta name="author" content="coteraes">
+
+		<meta name="viewport" content="width=device-width; initial-scale=1.0">
+
+		<!-- Replace favicon.ico & apple-touch-icon.png in the root of your domain and delete these references -->
+		<link rel="shortcut icon" href="/favicon.ico">
+		<link rel="apple-touch-icon" href="/apple-touch-icon.png">
+        <link href="sites/all/themes/custom/mitema/css/style.css" rel="stylesheet" type="text/css">
+	</head>
+
+	<body>
+		<div>
+			<div class="header">
+				<div class="logo">
+					<img src="sites/all/images/logo2.png" width="250px" height="250px">
+				</div>
+				<div class="titulo">
+					<h1>Página de Ejemplo</h1>
+				</div>
+
+			</div>
+			<div class="navbar">
+				<p>
+					<a href="/">Home</a>
+				</p>
+				<p>
+					<a href="/contact">Contact</a>
+				</p>
+			</div>
+
+			<div class="content">
+                        <h2>Bienvenido!</h2>
+			</div>
+
+			<div class="footer">
+				<p>
+					&copy; Copyright  by coteraes
+				</p>
+			</div>
+		</div>
+	</body>
+</html>
